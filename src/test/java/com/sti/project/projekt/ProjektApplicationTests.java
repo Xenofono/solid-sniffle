0bd1dd508d98ec777b36a8eb745ac07a14ba9155
Xenofono <kristoffer.nasstrom@gmail.com>
2019-10-24 10:53:12 +0200

    initial commit

---
+++ src/test/java/com/sti/project/projekt/ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sti.project.projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
