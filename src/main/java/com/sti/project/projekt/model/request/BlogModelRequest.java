fa558b1a4fbb0effa74920e370585f7fb6cbd0d5
Xenofono <kristoffer.nasstrom@gmail.com>
2019-10-25 11:22:38 +0200

    created http request model for blog

---
+++ src/main/java/com/sti/project/projekt/model/request/BlogModelRequest.java
@@ -0,0 +1,25 @@
+package com.sti.project.projekt.model.request;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+public class BlogModelRequest {
+
+    @NotNull(message = "Creator can't be null")
+    @Size(min = 2, message = "Name must be longer than 2")
+    private String creator;
+    @NotNull
+    private String content;
+    private LocalDateTime created;
+
+    public BlogModelRequest(String creator, String content) {
+        this.creator = creator;
+        this.content = content;
+        this.created = LocalDateTime.now();
+    }
+}
