428fa8d47ecd622564f454da87fe7a778408bec0
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2019-11-04 15:54:19 +0100

    added getById to client

--- src/main/java/com/sti/project/projekt/client/Client.java
+++ src/main/java/com/sti/project/projekt/client/Client.java
@@ -4,6 +4,7 @@ import com.sti.project.projekt.blog.model.response.BlogModelResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
@@ -19,10 +20,23 @@ public class Client {
 
     @GetMapping("/client")
     public String getAllBlogs(Model model) {
-        System.out.println("HEJ");
-        IReactiveDataDriverContextVariable reactiveContext = new ReactiveDataDriverContextVariable(webClient.get().uri("/")
+        IReactiveDataDriverContextVariable reactiveContext = new ReactiveDataDriverContextVariable(
+                webClient.get().uri("/")
                 .retrieve()
                 .bodyToFlux(BlogModelResponse.class));
+
+        model.addAttribute("blogs", reactiveContext);
+
+        return "blogs";
+    }
+
+    @GetMapping("/client/{id}")
+    public String getBlogById(@PathVariable String id, Model model) {
+        IReactiveDataDriverContextVariable reactiveContext = new ReactiveDataDriverContextVariable(
+                webClient.get().uri("/{id}", id)
+                        .retrieve()
+                        .bodyToFlux(BlogModelResponse.class));
+
         model.addAttribute("blogs", reactiveContext);
 
         return "blogs";
