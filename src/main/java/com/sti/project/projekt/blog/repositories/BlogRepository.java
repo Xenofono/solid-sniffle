f18743038bb7c9c9dad1b3c9108f549a70b437d4
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2019-11-06 18:15:36 +0100

    trying to deploy to heroku

--- src/main/java/com/sti/project/projekt/blog/repositories/BlogRepository.java
+++ src/main/java/com/sti/project/projekt/blog/repositories/BlogRepository.java
@@ -1,7 +1,10 @@
 package com.sti.project.projekt.blog.repositories;
 
 import com.sti.project.projekt.blog.entities.BlogEntity;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends ReactiveCrudRepository<BlogEntity, Long> {
 }
